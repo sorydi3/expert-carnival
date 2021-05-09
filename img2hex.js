2adf021a9e9cb25f4222530b058ffa911f0cebac
sorydi3 <ibrahimasory22@hotmail.com>
2021-05-09 16:27:51 +0100

    first commit

---
+++ img2hex.js
@@ -0,0 +1,15 @@
+//Require the file system accesses
+fs = require('fs');
+//Read the file Buffer
+imgReadBuffer =  fs.readFileSync('test-pattern.jpg');
+//Encode image buffer to hex
+imaHexEncode = new Buffer.from(imgReadBuffer).toString('hex');
+
+//output encoded data to console
+//console.log(imaHexEncode);
+
+
+//DECODE THE IMAGE
+var imgHexDecode = new Buffer(imaHexEncode,'hex');
+
+fs.writeFileSync('decodedImage.jpg',imaHexEncode);
